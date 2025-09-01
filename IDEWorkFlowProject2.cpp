29fbe11bc11f845b5b432d8200944ca6b1ed92c7
lweil <lweil@DESKTOP-2TLCSGG>
2025-09-01 15:30:19 -0400

    Add project files.

---
+++ IDEWorkFlowProject2.cpp
@@ -0,0 +1,15 @@
+// IDEWorkFlowProject2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char first, middle, last;
+	// I'm storing my respictive initials in these variables.
+	first = 'L';
+	middle = 'M';
+	last = 'W';
+	cout << first << middle << last;
+	return 0;
+}
